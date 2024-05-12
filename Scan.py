8cd9005122c8f61047a329d306905d3ab2eac72e
dark-cyber5 <rahman.h.z.2023@gmail.com>
2024-05-13 03:11:28 +0330

    update

--- Scan.py
+++ Scan.py
@@ -1,74 +1,69 @@
-import requests as req
+import requests
+import requests.adapters
+from urllib3.util.retry import Retry
 import json
 import typer
 import os
+import logging
 import TempMailClass
-import concurrent.futures
-
-
 
 class Criminalip:
+    def __init__(self):
+        self.result_path = "result"
+        if not os.path.exists(self.result_path):
+            os.mkdir(self.result_path)
+        self.session = requests.Session()
+        retries = Retry(total=50, backoff_factor=1)
+
+        adapter = requests.adapters.HTTPAdapter(max_retries=retries)
+        self.session.mount('http://', adapter)
+        self.session.mount('https://', adapter)
+
     def save_ip(self, ip_list, path: str, metasploit: bool):
         ip_list = list(set(ip_list))
         if metasploit:
-            ip_list = list(map(lambda x: x.replace("\n", " "), ip_list))
-        if os.path.exists("result"):
-            with open("result/" + path, "+a", encoding="utf-8") as file:
-                file.writelines(ip_list)  # type: ignore
-                typer.secho("Save Success", fg=typer.colors.YELLOW)
-        else:
-            os.mkdir("result")
-            self.save_ip(ip_list, path, metasploit)
+            ip_list = [x.replace("\n", " ") for x in ip_list]
+        full_path = os.path.join(self.result_path, path)
+        with open(full_path, "a", encoding="utf-8") as file:
+            file.writelines(ip_list)
+            typer.secho("Save Success", fg=typer.colors.YELLOW)
 
     def unduplicate(self, path: str):
-        if os.path.exists(path):
-            with open(path, "+r", encoding="utf-8") as file:
-                ip_list = file.readlines()
-            return ip_list
-        else:
-            return []
+        full_path = os.path.join(self.result_path, path)
+        if os.path.exists(full_path):
+            with open(full_path, "r", encoding="utf-8") as file:
+                return file.readlines()
+        return []
 
-    def get_ip(self, api_key,  query, path: str, metasploit,offset=1):
-        def run():
-            old_save = self.unduplicate(path)
-            ips = []
-            header = {"x-api-key": api_key}
-            while True:
-                try:
-                    typer.secho(f"\nGet Data From offset= {offset}", fg=typer.colors.GREEN)
-                    url = f"https://api.criminalip.io/v1/banner/search?query={query}&offset={offset}"
-                    response = req.get(url, headers=header).text
-                    pars = json.loads(response)
-                    tmp = list(
-                        map(
-                            lambda x: "http://"
-                            + x["ip_address"]
-                            + ":"
-                            + str(x["open_port_no"])
-                            + "\n",
-                            pars["data"]["result"],
-                        )
-                    )
-                    if len(tmp) > 0:
-                        if len(old_save) > 0:
-                            for ip in tmp:
-                                if not ip in old_save:
-                                    ips.append(ip)
-                        else:
-                            ips.extend(tmp)
-                    else:
-                        self.save_ip(ips, path, metasploit)
-                        break
-                    offset += 1
+    def get_ip(self, api_key, query, path: str, metasploit, offset=1):
+        self.session.headers.update({"x-api-key": api_key})
+        old_save = self.unduplicate(path)
+        ips = []
 
-                except:
-                    typer.secho(f"\nend \n ofsset= {offset}", fg=typer.colors.RED)
-                    self.save_ip(ips, path, metasploit)
-                    old_save.extend(ips)
-                    ips.clear()
+        while True:
+            try:
+                typer.secho(f"\nGet Data From offset= {offset}", fg=typer.colors.GREEN)
+                url = f"https://api.criminalip.io/v1/banner/search?query={query}&offset={offset}"
+                response = self.session.get(url)
+                response.raise_for_status()
+                pars = response.json()
+                tmp = [
+                    f"http://{x['ip_address']}:{x['open_port_no']}\n"
+                    for x in pars["data"]["result"]
+                ]
+
+                if not tmp:
+                    break
+
+                ips.extend([ip for ip in tmp if ip not in old_save])
+                offset += 1
+
+            except Exception as e:
+                typer.secho(f"\nEnd of data or an error occurred at offset = {offset}", fg=typer.colors.RED)
+                typer.secho(f"Error: {e}", fg=typer.colors.RED)
+                if 'x-api-key' in str(e):
                     api_key = TempMailClass.main()
-                    header["x-api-key"] = api_key 
-    
-        with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-            executor.map(run, range(offset,999999))
+                    self.session.headers.update({"x-api-key": api_key})
 
+        if ips:
+            self.save_ip(ips, path, metasploit)
